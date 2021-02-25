cb446d8b57214d55a9f5c8d8e4d4ced810989bb7
Mark Khramko <markkhramko@gmail.com>
2021-02-26 00:08:40 +0100

    Add example events labels

---
+++ events/labels/dom.js
@@ -0,0 +1,9 @@
+/*
+ * Export DOM events, which are used in your project.
+ * Full list can be found here: https://www.w3schools.com/jsref/dom_obj_event.asp
+ *
+ */
+
+// Scroll & resize events examples:
+export const SCROLL = "scroll";
+export const RESIZE = "resize";
